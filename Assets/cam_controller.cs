35858f79250c65dd98f6905a7cfd7b817316c293
Humphrey <chirambaht@gmail.com>
2022-07-06 16:53:58 +0200

    Sub changes to be tested

--- Assets/cam_controller.cs
+++ Assets/cam_controller.cs
@@ -2,7 +2,8 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class cam_controller : MonoBehaviour {
+public class cam_controller : MonoBehaviour
+{
     Vector3 touchStart;
     Vector3 mouseStart;
     const float zoomOutMin = 0.1f;
@@ -11,9 +12,9 @@ public class cam_controller : MonoBehaviour {
 
     Camera working_camera;
     Camera[] all_cameras = new Camera[3];
-    int camera_index =0;
-    int camera_count =0;
-    
+    int camera_index = 0;
+    int camera_count = 0;
+
     bool ui_show = true;
     GameObject text_ui;
     GameObject[] button_ui = new GameObject[2];
@@ -23,8 +24,9 @@ public class cam_controller : MonoBehaviour {
     // 1 - main camera      0.5 0   0.5 1
     // 2 - top camera       0   0.5 0.5 0.5
     // 3 - side camera      0   0   0.5 0.5
-	
-    void Start(){
+
+    void Start()
+    {
         camera_count = Camera.allCamerasCount;
         camera_index = 0;
         all_cameras = Camera.allCameras;
@@ -35,31 +37,36 @@ public class cam_controller : MonoBehaviour {
         button_ui[1] = GameObject.Find("big_cam");
     }
 
-    public void swap_camera(){
-         if (camera_index < camera_count - 1){
+    public void swap_camera()
+    {
+        if (camera_index < camera_count - 1)
+        {
             camera_index++;
         }
-        else{
+        else
+        {
             camera_index = 0;
         }
         working_camera = all_cameras[camera_index];
         tri_camera(false);
-        Debug.LogFormat("Camera changed to {0}/{1}:{2}", camera_index+1, camera_count, working_camera.name);
+        Debug.LogFormat("Camera changed to {0}/{1}:{2}", camera_index + 1, camera_count, working_camera.name);
     }
 
-	// Update is called once per frame
-	void Update () {
-         if (Input.GetKeyDown(KeyCode.C))
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.C))
         {
-           swap_camera();
-            
+            swap_camera();
         }
 
-        if(Input.GetMouseButtonDown(0)){
+        if (Input.GetMouseButtonDown(0))
+        {
             touchStart = working_camera.ScreenToWorldPoint(Input.mousePosition);
         }
 
-        if(Input.touchCount == 2){
+        if (Input.touchCount == 2)
+        {
             Touch touchZero = Input.GetTouch(0);
             Touch touchOne = Input.GetTouch(1);
 
@@ -72,34 +79,45 @@ public class cam_controller : MonoBehaviour {
             float difference = currentMagnitude - prevMagnitude;
 
             zoom(difference * 0.01f);
-        }else if(Input.GetMouseButton(0)){
+        }
+        else if (Input.GetMouseButton(0))
+        {
             Vector3 direction = touchStart - working_camera.ScreenToWorldPoint(Input.mousePosition);
             working_camera.transform.position += direction;
         }
-        else if(Input.GetMouseButton(1)){
+        else if (Input.GetMouseButton(1))
+        {
             float mag = (Input.mousePosition - mouseStart).magnitude;
-            if (Input.mousePosition.y > mouseStart.y ){
+            if (Input.mousePosition.y > mouseStart.y)
+            {
                 mag *= -1;
             }
-            working_camera.transform.Rotate(  new Vector3(mag,0,0)) ;
+            working_camera.transform.Rotate(new Vector3(mag, 0, 0));
             mouseStart = Input.mousePosition;
         }
         zoom(Input.GetAxis("Mouse ScrollWheel"));
-	}
-
-    void zoom(float increment){
-        increment = increment * zoomFactor;
-        working_camera.orthographicSize = Mathf.Clamp(working_camera.orthographicSize - increment, zoomOutMin, zoomOutMax);
     }
 
-    public void Ive_been_touched(){
+    void zoom(float increment)
+    {
+        increment = increment * zoomFactor;
+        working_camera.orthographicSize =
+            Mathf.Clamp(working_camera.orthographicSize - increment, zoomOutMin, zoomOutMax);
+    }
+
+    public void Ive_been_touched()
+    {
         Debug.Log("I've been touched");
     }
 
-    public void change_ui(){
-        if (ui_show){
+    public void change_ui()
+    {
+        if (ui_show)
+        {
             Debug.Log("Hiding the UI");
-        } else{
+        }
+        else
+        {
             Debug.Log("Showing the UI");
         }
         ui_show = !ui_show;
@@ -109,40 +127,55 @@ public class cam_controller : MonoBehaviour {
         // }
     }
 
-    public void tri_camera(bool to_change = true){
-        
+    public void tri_camera(bool to_change = true)
+    {
+
         // 3 - All 3 cameras    x   y   w   h
         // 0 - main camera      0.5 0   0.5 1
         // 1 - top camera       0   0.5 0.5 0.5
         // 2 - side camera      0   0   0.5 0.5
 
-        if (to_change){
+        if (to_change)
+        {
             three_cam_view = !three_cam_view;
         }
-        if (three_cam_view){
+        if (three_cam_view)
+        {
             Debug.Log("Showing 3 cameras");
             all_cameras[0].rect = new Rect(0.5f, 0f, 0.5f, 1f);
             all_cameras[1].rect = new Rect(0f, 0.5f, 0.5f, 0.5f);
             all_cameras[2].rect = new Rect(0f, 0f, 0.5f, 0.5f);
 
-            for (int i = 0; i < camera_count; i++){
+            for (int i = 0; i < camera_count; i++)
+            {
                 all_cameras[i].enabled = true;
             }
         }
-        else{
+        else
+        {
             Debug.Log("Showing 1 camera");
             all_cameras[0].rect = new Rect(0f, 0f, 1f, 1f);
             all_cameras[1].rect = new Rect(0f, 0f, 1f, 1f);
             all_cameras[2].rect = new Rect(0f, 0f, 1f, 1f);
 
-            for (int i = 0; i < camera_count; i++){
-                if (camera_index == i){
+            for (int i = 0; i < camera_count; i++)
+            {
+                if (camera_index == i)
+                {
                     all_cameras[i].enabled = true;
-                } else {
+                }
+                else
+                {
                     all_cameras[i].enabled = false;
                 }
             }
-            
         }
+    }
+
+    public void rotate_main_cam_by_180_degrees()
+    {
+        Camera.main.transform.Rotate(new Vector3(0, 0, 180));
+        Camera.main.transform.position = new Vector3(Camera.main.transform.position.x, Camera.main.transform.position.y,
+                                                     Camera.main.transform.position * -1);
     }
 }
